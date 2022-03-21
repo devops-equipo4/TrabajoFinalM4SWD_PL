e6ba6d8e2d6de16f681e5093d5e7ececa8f5ed6e
Nestor Fuenzalida <nestor.fuenzalida@gmail.com>
2022-03-20 21:26:31 -0300

    test pipeline

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -7,12 +7,9 @@ def call(stages){
             'compile'         : 'sCompile',
             'unitTest'        : 'sUnitTest',
             'jar'             : 'sJar',
-            'sonar'           : 'sSonar',
-            'nexusUpload'     : 'sNexusUpload',
             'gitCreateRelease': 'sGitCreateRelease',
     ]
 
-    def validations = new validate.validations();
     def stagesArray = []
     stagesArray = validations.searchKeyInArray(stages, ";", listStagesOrder)
 
@@ -30,8 +27,6 @@ def allStages() {
     sCompile()
     sUnitTest()
     sJar()
-    sSonar()
-    sNexusUpload()
     sGitCreateRelease()
 }
 
@@ -56,49 +51,12 @@ def sJar() {
     }
 }
 
-def sSonar() {
-    env.STAGE = "Stage Sonar"
-    stage("$env.STAGE") {
-        withSonarQubeEnv('sonarqube') {
-            sh "mvn clean verify sonar:sonar -Dsonar.projectKey=$env.GIT_REPO_NAME-$env.BRANCH-$env.BUILD_NUMBER -Dsonar.java.binaries=build"
-        }
-    }
-}
-
-def sNexusUpload() {
-    env.STAGE = "Stage Nexus Upload"
-    stage("$env.STAGE") {
-        nexusPublisher nexusInstanceId: 'nexus',
-                nexusRepositoryId: 'devops-usach-nexus',
-                packages: [
-                        [$class         : 'MavenPackage',
-                         mavenAssetList : [
-                                 [classifier: '',
-                                  extension : 'jar',
-                                  filePath  : 'build/DevOpsUsach2020-0.0.1.jar'
-                                 ]
-                         ],
-                         mavenCoordinate: [
-                                 artifactId: 'DevOpsUsach2020',
-                                 groupId   : 'com.devopsusach2020',
-                                 packaging : 'jar',
-                                 version   : '0.0.1'
-                         ]
-                        ]
-                ]
-    }
-}
-
 def sGitCreateRelease() {
     env.STAGE = "Stage Git Create Release"
     stage("Stage Git Create Release") {
-        if (env.GIT_BRANCH =~ "develop*") {
-            sh "git checkout develop"
-            sh "git checkout -b release-v$env.BUILD_NUMBER-0-0"
-            sh "git push --set-upstream origin release-v$env.BUILD_NUMBER-0-0"
-        }else{
-            sh 'echo "Branch del tipo feature"'
-        }
+        sh "git checkout feat_jenkins"
+        sh "git checkout -b release-v1-0-0"
+        sh "git push --set-upstream origin release-v1-0-0"
     }
 }
 return this;
